734f1a65f9a6592b1f53a526f164a042ca400399
Sahil Harpal <56965873+Sahil1479@users.noreply.github.com>
2021-12-28 10:25:10 +0530

    Update views.py

--- events/views.py
+++ events/views.py
@@ -12,23 +12,3 @@ def events(request, type):
 def event(request, type, eventid):
     event = get_object_or_404(Event, pk=eventid)
     return render(request, 'event.html', {'event': event})
-
-
-# @login_required
-# def registered(request):
-#     events = request.user.events.all()
-#     types = ['technical', 'informal', 'workshop']
-#     categories = []
-#     for type in types:
-#         if(request.user.events.filter(type=type).exists()):
-#             categories.append(type)
-#     return render(request, 'registered.html', {'events' : events, 'categories':categories})
-
-# @login_required
-# def register_for_event(request, eventid):
-#     event = Event.objects.get(pk=eventid)
-#     if(not event.registration_open):
-#         return redirect('event', event.type, event.pk)
-#     request.user.events.add(event)
-#     messages.success(request, f"Successfully registered for '{event.name}'.")
-#     return redirect(user_profile)
