febb4b2f6af8511da40857b2ef862ee2d34c8434
Aryan Garg <garg2001aryan@gmail.com>
2022-01-22 18:08:00 +0530

    Registration bugs fixed

--- users/views.py
+++ users/views.py
@@ -12,54 +12,46 @@ from .forms import TeamCreationForm, TeamJoiningForm, EditTeamForm
 User = get_user_model()
 
 
+def isRegistrationFormValid(data):
+    requiredFields = ['first_name', 'last_name', 'email', 'college_name', 'phone_no', 'gender', 'city', 'current_year']
+    for item in requiredFields:
+        if item not in data:
+            return False
+    return True
+
 @login_required
 def user_profile(request):
     if(request.method == "POST"):
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-        college_name = request.POST['college_name']
-        phone_no = request.POST['phone_no']
-        gender = request.POST['gender']
-        city = request.POST['city']
-        current_year = request.POST['current_year']
-        isCampusAmbassador = request.POST.get('isCampusAmbassador', False)
-        referralCode = request.POST.get('referral_code', 'default')
-        user = request.user
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save()
-        extendeduser = ExtendedUser.objects.filter(user=request.user).first()
-        # If the current user is using referral code.
-        if referralCode and referralCode != "default":
-            referredBy = ExtendedUser.objects.filter(invite_referral=referralCode).first()
-            if referredBy:
-                extendeduser.referred_by = referredBy.user
-            else:
-                messages.error(request, 'No referral code found, either check your code again or leave the field empty.')
-                return redirect("/users/profile")
-        extendeduser.gender = gender
-        extendeduser.contact = phone_no
-        extendeduser.current_year = gender
-        extendeduser.college = college_name
-        extendeduser.city = city
-        extendeduser.current_year = current_year
-        if isCampusAmbassador == "on":
-            extendeduser.ambassador = True
-        # If the user wants to be the campus ambassador.
-        invite_referral = 'CA' + str(uuid.uuid4().int)[:6]
-        if extendeduser.isProfileCompleted is False and extendeduser.ambassador is True:
-            invite_referral = 'CA' + str(uuid.uuid4().int)[:6]
-            extendeduser.invite_referral = invite_referral
-            send_mail(
-                'Campus Ambassador',
-                f"Dear {user.first_name},\nYou are now a campus ambassador. Your referral code is {invite_referral}.\nRegards,\nPrometeo 2022 Team",
-                'iitj.iotwebportal@gmail.com',
-                [user.email],
-                fail_silently=False,
-            )
-        extendeduser.isProfileCompleted = True
-        extendeduser.save()
-        messages.success(request, 'Your profile has been updated.')
+        data = request.POST
+        if isRegistrationFormValid(data):
+            user = request.user
+            user.first_name = data['first_name']
+            user.last_name = data['last_name']
+
+            extendeduser = ExtendedUser.objects.filter(user=request.user).first()
+            extendeduser.gender = data['gender']
+            extendeduser.contact = data['phone_no']
+            extendeduser.current_year = data['gender']
+            extendeduser.college = data['college_name']
+            extendeduser.city = data['city']
+            extendeduser.current_year = data['current_year']
+            if 'referral_code' in data and data['referral_code']!='' and extendeduser.isProfileCompleted is False:
+                referralCode = data['referral_code']
+                if ExtendedUser.objects.filter(invite_referral=referralCode).exists():
+                    referredBy = ExtendedUser.objects.filter(invite_referral=referralCode).first()
+                    extendeduser.referred_by = referredBy.user
+                else:
+                    messages.info(request, 'Invalid Referral Code.')
+                    return render(request, 'profile.html')
+            
+            extendeduser.isProfileCompleted = True
+            extendeduser.save()
+            user.save()
+            messages.info(request, 'Your profile has been updated.')
+            return redirect('/')
+
+        else:
+            messages.info(request, 'Fill all the required fields.')
 
     return render(request, 'profile.html')
 
