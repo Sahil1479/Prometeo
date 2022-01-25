2f5fb0bf173e9df59e83272e1037ed9e6085b6be
unknown <santosh.2@iitj.ac.in>
2022-01-25 16:40:09 +0530

    linting errors

--- prometeo/urls.py
+++ prometeo/urls.py
@@ -18,5 +18,4 @@ if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if not settings.DEBUG:
-    urlpatterns = urlpatterns + [url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})]
-
+    urlpatterns = urlpatterns + [url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}), url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})]
