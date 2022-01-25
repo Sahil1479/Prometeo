d3d5bc7d0cd3e5ccc233189efbdfb9ad8e89f5e2
unknown <santosh.2@iitj.ac.in>
2022-01-25 16:29:13 +0530

    staticfiles setup

--- prometeo/urls.py
+++ prometeo/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
@@ -10,4 +12,11 @@ urlpatterns = [
     path('coordinator/', include('coordinator.urls')),
     path('accounts/', include('allauth.urls')),
     path('users/', include('users.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns = urlpatterns + [url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})]
+
