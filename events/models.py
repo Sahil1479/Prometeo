d99e06a8cd7904cf12d2f11b2a8430061a5c3b07
Aryan Garg <garg2001aryan@gmail.com>
2022-02-11 16:31:52 +0530

    Events model updated

--- events/models.py
+++ events/models.py
@@ -27,40 +27,35 @@ class Brochure(models.Model):
 
 
 class Event(models.Model):
-    # Event Details
     name = models.CharField(max_length=50, verbose_name="Event Name", unique=True, blank=False, null=False)
-    type = models.CharField(max_length=30, choices=EVENT_CHOICES, default='event', verbose_name='Event Type')
     speaker = models.CharField(max_length=50, null=True, blank=True, verbose_name="If Talk mention speaker's name (Else leave empty)")
-    designation = models.CharField(max_length=50, null=True, blank=True, verbose_name="Speaker Designation (only for Talk)")
-    description = models.TextField(max_length=5000, null=True, blank=True, verbose_name="Event Description (Write more for speaker description)")
-    prize = models.CharField(max_length=100, verbose_name="Prize Money (Rs.)", null=True, blank=True, default="Prizes worth Rs.")
-    external_link = models.URLField(max_length=500, null=True, blank=True, verbose_name="External Link for Registration")
-    venue = models.CharField(max_length=50, null=True, blank=True, verbose_name="Event Venue")
-    featured = models.BooleanField(verbose_name="Display on home page", default=False, blank=True)
-    rank = models.IntegerField(blank=False, null=False, default=1)
-
-    # Event Registration Details
+    designation = models.CharField(max_length=200, null=True, blank=True, verbose_name="Speaker Designation (only for Talk)")
+    image = models.ImageField(upload_to="images/", verbose_name="Cover Image(prefer uploading square images)(for speakers upload images with more padding, i.e, face in the center)", blank=False, null=False)
+    rulebook = models.FileField(upload_to="rulebooks/", null=True, blank=True, verbose_name="Rulebook File")
+    sponsor_image1 = models.ImageField(upload_to="images/", null=True, blank=True, verbose_name="Sponser Image 1(upload rectangular images)")
+    sponsor_website = models.URLField(max_length=1000, null=True, blank=True, verbose_name="Link to Sponsors Website")
     participation_type = models.CharField(max_length=25, choices=EVENT_PARTICIPATION, default='individual', verbose_name="Participation Type")
     min_team_size = models.IntegerField(verbose_name="Minimum Team Size (leave unchanged for individual event)", default=1)
     max_team_size = models.IntegerField(verbose_name="Maximum Team Size (leave unchanged for individual event)", default=1)
-    registration_open = models.BooleanField(verbose_name="Registrations Open", default=True, blank=True)
-    submission_link = models.URLField(max_length=500, null=True, blank=True, verbose_name="Event submission link")
-
-    # Event Dates
+    prize = models.CharField(max_length=200, verbose_name="Prize Money (Rs.)", null=True, blank=True, default="Prize <Enter prize money here> INR")
+    description = models.TextField(max_length=5000, null=True, blank=True, verbose_name="Event Description (Write more for speaker description)")
+    host = models.CharField(max_length=50, null=True, blank=True, verbose_name="Event Host")
+    external_link = models.URLField(max_length=500, null=True, blank=True, verbose_name="External Link for Registration")
     date = models.DateField(verbose_name="Event Date (Leave unchanged if the date is not decided)", null=False, blank=False, default="2023-01-01")
     time = models.TimeField(null=False, blank=False, verbose_name="Event Time")
     end_date = models.DateField(verbose_name="Event end Date", null=False, blank=False, default="2023-01-01")
     end_time = models.TimeField(null=False, blank=False, verbose_name="Event end Time")
+    venue = models.CharField(max_length=50, null=True, blank=True, verbose_name="Event Venue")
+    registration_open = models.BooleanField(verbose_name="Registrations Open", default=True, blank=True)
+    type = models.CharField(max_length=30, choices=EVENT_CHOICES, default='event', verbose_name='Event Type')
     event_started = models.BooleanField(verbose_name="Event started", default=False, blank=True)
-
-    # Streaming Links
     meet_link = models.URLField(max_length=500, null=True, blank=True, verbose_name="meet Link for streaming")
     youtube_link = models.URLField(max_length=500, null=True, blank=True, verbose_name="youtube Link for streaming")
     webx_link = models.URLField(max_length=500, null=True, blank=True, verbose_name="webX Link for streaming")
+    featured = models.BooleanField(verbose_name="Display on home page", default=False, blank=True)
+    rank = models.IntegerField(blank=False, null=False, default=1)
 
-    # Event Uploads
-    image = models.ImageField(upload_to="images/", verbose_name="Cover Image(prefer uploading square images)(for speakers upload images with more padding, i.e, face in the center)", blank=False, null=False)
-    rulebook = models.FileField(upload_to="rulebooks/", null=True, blank=True, verbose_name="Rulebook File")
+    submission_link = models.URLField(max_length=500, null=True, blank=True, verbose_name="Event submission link")
     material_name = models.CharField(max_length=100, null=True, blank=True, verbose_name="Material Name")
     material = models.FileField(upload_to="supplementaryMaterials/", null=True, blank=True, verbose_name="Supplementary Material")
 
