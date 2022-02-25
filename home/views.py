53ce35999db7965ef4ea337043afa7b195399ec8
Joel Thomas <thomas.2@iitj.ac.in>
2022-02-26 04:30:21 +0530

    added initiatives page

--- home/views.py
+++ home/views.py
@@ -45,6 +45,10 @@ def nsd(request):
     event = Event.objects.filter(name='National Science Day').first()
     return render(request, 'national_science_day.html', {'event': event, 'brochure': brochure})
 
+def initiatives(request):
+    event = Event.objects.filter(type='initiative')
+    return render(request, 'initiative.html', {'events': event})
+
 
 def sponsors(request):
     if registrationNotCompleted(request):
